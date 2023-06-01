b839558fd22b1ce4a378c0b429e04c18025e8685
Arien Mungongo <107950734+AfroZen-7@users.noreply.github.com>
2023-06-01 15:38:29 +0200

    Add files via upload

---
+++ metrics.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -f "/var/ossec/wazuh_manager_metrics.json" ]; then
+    rm -f "/var/ossec/wazuh_manager_metrics.json"
+fi
+
+host=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+ram=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }' | sed 's/[ \t]*$//')
+disk=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}' | sed 's/[ \t]*$//')
+cpu=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}' | sed 's/[ \t]*$//')
+json='{"host":"'"$host"'", "ram":"'"$ram"'", "cpu":"'"$cpu"'", "disk":"'"$disk"'"}'
+echo -e "$json" >> /var/ossec/wazuh_manager_metrics.json
